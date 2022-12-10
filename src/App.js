7bd81ad1b1dc315e8234fec83bb12723783abf63
rushi2022 <191200107018@asoit.edu.in>
2022-12-10 19:31:41 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
 function App() {
+  const [name, setFname] = useState(["jhon", "Cersei"]);
+  const arr = ["jhon", "Cersei"];
+
+  const adeListerner = () => {
+    setFname(arr.sort());
+  };
+  const dadeListerner = () => {
+    setFname(arr.sort((a, b) => b - a));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={adeListerner}>Sort ADE</button>
+      <button onClick={dadeListerner}>Sort DADE</button>
+      <table>
+        <tr>
+          <td>First Name</td>
+
+          <td>Last Name</td>
+
+          <td>Age</td>
+
+          <td>FullName</td>
+        </tr>
+        <tr>
+          <td>{name[0]}</td>
+
+          <td>Snow</td>
+
+          <td>35</td>
+
+          <div
+            style={name[0] === "jhon" ? { color: "green" } : { color: "red" }}
+          >
+            <td>{name[0]} Snow</td>
+          </div>
+        </tr>
+        <tr>
+          <td>{name[1]}</td>
+
+          <td>Targerian</td>
+
+          <td>42</td>
+          <div
+            style={name[1] === "Cersei" ? { color: "green" } : { color: "red" }}
+          >
+            <td>{name[1]} Targerian</td>
+          </div>
+        </tr>
+      </table>
     </div>
   );
 }
